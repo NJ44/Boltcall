8868eac4cc1cfd84f78767cde5638a3adb400468
unknown <noamyakoby6@gmail.com>
2026-04-01 10:43:30 +0300

    auto: feature-section-with-bento-grid.tsx

--- src/components/ui/feature-section-with-bento-grid.tsx
+++ src/components/ui/feature-section-with-bento-grid.tsx
@@ -208,7 +208,7 @@ function Feature() {
                 </p>
               </div>
               <div className="flex-1 flex items-center justify-center">
-                <div className="w-48 h-48 md:w-72 md:h-72">
+                <div className="w-56 h-56 md:w-80 md:h-80">
                   <LazyLottie
                     src="/costumer_statisfication.lottie"
                     loop
