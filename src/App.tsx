0f20c3434a8e4ba98305194a22a6822d7b95243e
unknown <noamyakoby6@gmail.com>
2026-04-05 17:02:16 +0300

    auto: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import AppRoutes from './routes/AppRoutes';
 import PWAUpdatePrompt from './components/PWAUpdatePrompt';
 import OfflineBanner from './components/OfflineBanner';
+import { ToastProvider } from './contexts/ToastContext';
 
 function App() {
   return (
-    <>
+    <ToastProvider>
       <OfflineBanner />
       <AppRoutes />
       <PWAUpdatePrompt />
-    </>
+    </ToastProvider>
   );
 }
 
