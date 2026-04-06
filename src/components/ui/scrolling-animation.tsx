1e2c3bc01ea980ce16564e101e206f3cda4211c9
unknown <noamyakoby6@gmail.com>
2026-04-06 21:51:46 +0300

    auto: scrolling-animation.tsx

--- src/components/ui/scrolling-animation.tsx
+++ src/components/ui/scrolling-animation.tsx
@@ -47,7 +47,7 @@ export function ScrollingAnimation({ onNavigate }: ScrollingAnimationProps) {
   const startAngle = -Math.PI / 2
 
   return (
-    <div ref={sectionRef} className="min-h-[150vh]">
+    <div ref={sectionRef} className="min-h-[130vh]">
       <div className="h-screen flex items-center justify-center p-8 sticky top-0">
         <div className="relative">
           {/* Outer ring */}
