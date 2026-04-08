ea286fdbaeec9d1d92fad384d44fbd2cd67f9362
unknown <noamyakoby6@gmail.com>
2026-04-08 16:50:31 +0300

    auto: SEOAuditPDF.tsx

--- src/pages/SEOAuditPDF.tsx
+++ src/pages/SEOAuditPDF.tsx
@@ -216,6 +216,7 @@ const SEOAuditPDF: React.FC = () => {
       {/* Hero */}
       <section className="relative pt-32 pb-4 bg-gradient-to-br from-blue-50 via-white to-blue-50/30">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+          <Breadcrumbs />
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
