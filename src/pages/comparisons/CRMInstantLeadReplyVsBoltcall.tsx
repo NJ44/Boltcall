1f2f70794ac9b4df72d85556d500c4e78fdc1187
unknown <noamyakoby6@gmail.com>
2026-04-10 10:33:32 +0300

    auto: CRMInstantLeadReplyVsBoltcall.tsx

--- src/pages/comparisons/CRMInstantLeadReplyVsBoltcall.tsx
+++ src/pages/comparisons/CRMInstantLeadReplyVsBoltcall.tsx
@@ -408,6 +408,7 @@ const CRMInstantLeadReplyVsBoltcall: React.FC = () => {
 
         {/* Channel Comparison */}
         <motion.section
+          id="multi-channel"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, delay: 0.5 }}
