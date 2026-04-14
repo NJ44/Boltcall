378f52bcc040c112328cfad41566756e00674456
unknown <noamyakoby6@gmail.com>
2026-04-14 16:52:13 +0300

    auto: pricing-table.tsx

--- src/components/ui/pricing-table.tsx
+++ src/components/ui/pricing-table.tsx
@@ -207,7 +207,13 @@ export function PricingTable({
                       <svg className="w-3 h-3 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20">
                         <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                       </svg>
-                      We Set It Up For You
+                      Done For You
+                    </div>
+                    <div className="flex items-center text-xs text-gray-900">
+                      <svg className="w-3 h-3 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20">
+                        <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                      </svg>
+                      Ads Campaigns
                     </div>
                     <div className="flex items-center text-xs text-gray-900">
                       <svg className="w-3 h-3 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20">
