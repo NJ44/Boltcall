8117de05895898db41f07b6b9190023fc862b90d
unknown <noamyakoby6@gmail.com>
2026-04-16 12:03:13 +0300

    auto: DashboardPage.tsx

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Phone } from 'lucide-react';
 import { useSearchParams } from 'react-router-dom';
 import confetti from 'canvas-confetti';
