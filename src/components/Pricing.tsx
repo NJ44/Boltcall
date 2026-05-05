1a48f58beb47d8f30b9df50e13d3bc1f75c0add6
Noam <noamyakoby6@gmail.com>
2026-05-05 11:08:48 +0300

    auto: Pricing.tsx

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
 import Section from './ui/Section';
 import { PricingTable } from './ui/pricing-table';
